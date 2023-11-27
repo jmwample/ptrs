2b0cc68cbace21f987d2b3dfaf5ea74ee700a04c
jmwample <8297368+jmwample@users.noreply.github.com>
2023-11-27 00:31:41 -0700

    checkpoint commit - framing

--- src/obfs4/framing/mod.rs
+++ src/obfs4/framing/mod.rs
@@ -33,26 +33,29 @@
 /// and the initial counter value.  It is imperative that the counter does not
 /// wrap, and sessions MUST terminate before 2^64 frames are sent.
 
-use crate::common::drbg::{self, Drbg};
+use crate::common::drbg::{self, Drbg, Seed};
 
 use tokio::io::{AsyncRead, AsyncWrite, AsyncReadExt, AsyncWriteExt};
 use tokio_util::codec::{Encoder,Decoder};
 use bytes::{BytesMut, Buf};
 use futures::{Sink, Stream, StreamExt, SinkExt};
+use crypto_secretbox::{
+    aead::{AeadCore, Aead, KeyInit, OsRng, generic_array::GenericArray},
+    XSalsa20Poly1305, Nonce,
+};
 
-
-// MaximumSegmentLength is the length of the largest possible segment
-// including overhead.
+/// MaximumSegmentLength is the length of the largest possible segment
+/// including overhead.
 const MAXIMUM_SEGMENT_LENGTH: usize = 1500 - (40 + 12);
 
-// secre box overhead is fixed length prefix and counter
+/// secret box overhead is fixed length prefix and counter
 const SECRET_BOX_OVERHEAD: usize = NONCE_PREFIX_LENGTH + NONCE_COUNTER_LENGTH;
 
-// FrameOverhead is the length of the framing overhead.
+/// FrameOverhead is the length of the framing overhead.
 const FRAME_OVERHEAD: usize = LENGTH_LENGTH + SECRET_BOX_OVERHEAD;
 
-// MaximumFramePayloadLength is the length of the maximum allowed payload
-// per frame.
+/// MaximumFramePayloadLength is the length of the maximum allowed payload
+/// per frame.
 const MAXIMUM_FRAME_PAYLOAD_LENGTH: usize = MAXIMUM_SEGMENT_LENGTH - FRAME_OVERHEAD;
 
 
@@ -67,32 +70,12 @@ const LENGTH_LENGTH: usize = 2;
 
 const KEY_LENGTH: usize = 32;
 
-// KeyLength is the length of the Encoder/Decoder secret key.
+/// KeyLength is the length of the Encoder/Decoder secret key.
 const KEY_MATERIAL_LENGTH: usize = KEY_LENGTH + NONCE_PREFIX_LENGTH + drbg::SEED_LENGTH;
 
 
-/// 
-struct NonceBox {
-    prefix: [u8; NONCE_PREFIX_LENGTH],
-    counter: u64,
-}
-
-/// Encoder is a frame encoder instance.
-struct Obfs4Encoder {
-    key: [u8; KEY_LENGTH],
-    nonce: NonceBox,
-    drbg: Drbg
-}
-impl Obfs4Encoder {
-    fn new() -> Self {
-        Self {
-        }
-    }
-}
-
 ///Decoder is a frame decoder instance.
 struct Obfs4Decoder {
-    key: [u8; KEY_LENGTH],
     nonce: NonceBox,
     drbg: Drbg,
 
@@ -102,120 +85,287 @@ struct Obfs4Decoder {
 }
 
 impl Obfs4Decoder {
-    fn new() -> Self {
+    // Creates a new Decoder instance.  It must be supplied a slice
+    // containing exactly KeyLength bytes of keying material.
+    fn new(key_material: [u8; KEY_MATERIAL_LENGTH]) -> Self {
+        let mut key: [u8; KEY_LENGTH] =  key_material[..KEY_LENGTH].try_into().unwrap();
+        let nonce = NonceBox::new(&key_material[KEY_LENGTH .. (KEY_LENGTH + NONCE_PREFIX_LENGTH)]);
+        let seed = Seed::try_from(&key_material[(KEY_LENGTH+NONCE_PREFIX_LENGTH)..]).unwrap();
+        let d = Drbg::new(Some(seed)).unwrap();
+
         Self {
+            drbg: d,
+            nonce,
+
+            next_nonce: [0_u8; NONCE_LENGTH],
+            next_length: 0,
+            next_length_inalid: false,
         }
     }
 }
 
 struct Obfs4Codec {
+    key: [u8; KEY_LENGTH],
     encoder: Obfs4Encoder,
     decoder: Obfs4Decoder,
 }
 
 impl Obfs4Codec {
-    fn new() -> Self {
+    fn new(key_material: [u8; KEY_MATERIAL_LENGTH]) -> Self {
+        let mut key: [u8; KEY_LENGTH] =  key_material[..KEY_LENGTH].try_into().unwrap();
         Self {
-            encoder: Obfs4Encoder::new(),
-            decoder: Obfs4Decoder::new(),
+            key,
+            encoder: Obfs4Encoder::new(key_material),
+            decoder: Obfs4Decoder::new(key_material),
         }
     }
 }
 
-
 enum Obfs4Message {
     ClientHandshake,
     ServerHandshake,
-    ProxyMessage,
+    ProxyPayload(Vec<u8>),
 }
 
 impl Decoder for Obfs4Codec {
     type Item = Obfs4Message;
-    type Error = std::io::Error;
+    type Error = FrameError;
 
+    // Decode decodes a stream of data and returns the length if any.  ErrAgain is
+    // a temporary failure, all other errors MUST be treated as fatal and the
+    // session aborted.
     fn decode(
         &mut self,
         src: &mut BytesMut
     ) -> std::result::Result<Option<Self::Item>, Self::Error> {
-        if src.len() < 4 {
-            // Not enough data to read length marker.
-            return Ok(None);
+        // A length of 0 indicates that we do not know the expected size of
+        // the next frame.
+        if self.decoder.next_length == 0 {
+            // Attempt to pull out the next frame length
+            if LENGTH_LENGTH > src.len() {
+                return Ok(None)
+            }
+
+            // Remove the field length from the buffer
+            let mut len_buf: [u8; LENGTH_LENGTH] = src[..LENGTH_LENGTH].try_into().unwrap();
+
+            // derive the nonce that the peer would have used
+            self.decoder.next_nonce = self.decoder.nonce.next()?;
+
+            // De-obfuscate the length field
+            let mut length = u16::from_be_bytes(len_buf);
+            let length_mask = self.decoder.drbg.uint64() as u16;
+            length ^= length_mask;
+            if MAX_FRAME_LENGTH < length as usize || MIN_FRAME_LENGTH > length as usize {
+                // Per "Plaintext Recovery Attacks Against SSH" by
+                // Martin R. Albrecht, Kenneth G. Paterson and Gaven J. Watson,
+                // there are a class of attacks againt protocols that use similar
+                // sorts of framing schemes.
+                //
+                // While obfs4 should not allow plaintext recovery (CBC mode is
+                // not used), attempt to mitigate out of bound frame length errors
+                // by pretending that the length was a random valid range as pe
+                // the countermeasure suggested by Denis Bider in section 6 of the
+                // paper.
+
+                self.decoder.next_length_inalid = true;
+                getrandom::getrandom(&mut len_buf);
+                length = u16::from_be_bytes(len_buf) % (MAX_FRAME_LENGTH-MIN_FRAME_LENGTH)as u16 + MIN_FRAME_LENGTH as u16;
+            }
+
+            self.decoder.next_length = length;
         }
 
-        // Read length marker.
-        let mut length_bytes = [0u8; 4];
-        length_bytes.copy_from_slice(&src[..4]);
-        let length = u32::from_le_bytes(length_bytes) as usize;
+        let next_len = self.decoder.next_length as usize;
 
-        // Check that the length is not too large to avoid a denial of
-        // service attack where the server runs out of memory.
-        if length > MAX {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::InvalidData,
-                format!("Frame of length {} is too large.", length)
-            ));
-        }
-
-        if src.len() < 4 + length {
+        if next_len > src.len() {
             // The full string has not yet arrived.
             //
             // We reserve more space in the buffer. This is not strictly
             // necessary, but is a good idea performance-wise.
-            src.reserve(4 + length - src.len());
+            src.reserve(next_len - src.len());
 
             // We inform the Framed that we need more bytes to form the next
             // frame.
             return Ok(None);
         }
 
-        // Use advance to modify src such that it no longer contains
-        // this frame.
-        let data = src[4..4 + length].to_vec();
-        src.advance(4 + length);
+        // Use advance to modify src such that it no longer contains this frame.
+        let data = src[2..2+next_len].to_vec();
+        src.advance(2 + next_len);
 
-        // Convert the data to a string, or fail if it is not valid utf-8.
-        match String::from_utf8(data) {
-            Ok(string) => Ok(Some(string)),
-            Err(utf8_error) => {
-                Err(std::io::Error::new(
-                    std::io::ErrorKind::InvalidData,
-                    utf8_error.utf8_error(),
-                ))
-            },
+
+        // Unseal the frame
+        let key = GenericArray::from_slice(&self.key);
+        let cipher = XSalsa20Poly1305::new(&key);
+        let nonce = GenericArray::from_slice(&self.decoder.next_nonce); // unique per message
+
+        let plaintext = cipher.decrypt(&nonce, data.as_ref())?;
+
+        // Clean uo and prepare for the next frame
+        self.decoder.next_length = 0;
+        self.decoder.nonce.counter += 1;
+
+        Ok(Some(Obfs4Message::ProxyPayload(plaintext)))
+    }
+}
+
+/// Encoder is a frame encoder instance.
+struct Obfs4Encoder {
+    nonce: NonceBox,
+    drbg: Drbg
+}
+
+impl Obfs4Encoder {
+    /// Creates a new Encoder instance. It must be supplied a slice
+    /// containing exactly KeyLength bytes of keying material.  
+    fn new(key_material: [u8; KEY_MATERIAL_LENGTH]) -> Self {
+        let mut key: [u8; KEY_LENGTH] =  key_material[..KEY_LENGTH].try_into().unwrap();
+        let nonce = NonceBox::new(&key_material[KEY_LENGTH .. (KEY_LENGTH + NONCE_PREFIX_LENGTH)]);
+        let seed = Seed::try_from(&key_material[(KEY_LENGTH + NONCE_PREFIX_LENGTH)..]).unwrap();
+        let d = Drbg::new(Some(seed)).unwrap();
+
+        Self {
+            nonce,
+            drbg: d,
         }
     }
 }
 
 
 impl Encoder<String> for Obfs4Codec {
-    type Error = std::io::Error;
+    type Error = FrameError;
 
+    /// Encode encodes a single frame worth of payload and returns the encoded
+    /// length.  InvalidPayloadLengthError is recoverable, all other errors MUST be
+    /// treated as fatal and the session aborted.
     fn encode(&mut self, item: String, dst: &mut BytesMut) -> std::result::Result<(), Self::Error> {
-        // Don't send a string if it is longer than the other end will
-        // accept.
-        if item.len() > MAX {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::InvalidData,
-                format!("Frame of length {} is too large.", item.len())
-            ));
+        // Don't send a string if it is longer than the other end will accept.
+        if MAXIMUM_SEGMENT_LENGTH < item.len() {
+            return Err(FrameError::InvalidPayloadLength(item.len()));
+        }
+        if dst.len() < item.len() + FRAME_OVERHEAD {
+            return Err(FrameError::ShortBuffer);
         }
 
-        // Convert the length into a byte array.
-        // The cast to u32 cannot overflow due to the length check above.
-        let len_slice = u32::to_le_bytes(item.len() as u32);
+        // Generate a new nonce
+        let nonce_bytes = self.encoder.nonce.next()?;
+
+        // Encrypt and MAC payload
+        let key = GenericArray::from_slice(&self.key);
+        let cipher = XSalsa20Poly1305::new(&key);
+        let nonce = GenericArray::from_slice(&nonce_bytes); // unique per message
+
+        let ciphertext = cipher.encrypt(&nonce, item.into_bytes().as_ref())?;
+
+        // Obfuscate the length
+        let mut length = ciphertext.len() as u16;
+        let length_mask: u16 = self.encoder.drbg.uint64() as u16;
+        length ^= length_mask;
 
         // Reserve space in the buffer.
-        dst.reserve(4 + item.len());
+        dst.reserve(ciphertext.len() + LENGTH_LENGTH);
 
         // Write the length and string to the buffer.
-        dst.extend_from_slice(&len_slice);
-        dst.extend_from_slice(item.as_bytes());
+        dst.extend_from_slice(&length.to_be_bytes());
+        dst.extend_from_slice(&ciphertext);
         Ok(())
+    }
+}
+
+
+/// internal nonce management for NaCl secret boxes
+struct NonceBox {
+    prefix: [u8; NONCE_PREFIX_LENGTH],
+    counter: u64,
+}
+
+impl NonceBox {
+    pub fn new(prefix: impl AsRef<[u8]>) -> Self {
+        assert!(prefix.as_ref().len() > NONCE_PREFIX_LENGTH);
+        Self {
+            prefix: prefix.as_ref()[..NONCE_PREFIX_LENGTH].try_into().unwrap(),
+            counter: 1,
+        }
+    }
+
+    pub fn next(&mut self) -> std::result::Result<[u8; NONCE_LENGTH], FrameError> {
+        // The security guarantee of Poly1305 is broken if a nonce is ever reused
+        // for a given key.  Detect this by checking for counter wraparound since
+        // we start each counter at 1.  If it ever happens that more than 2^64 - 1
+        // frames are transmitted over a given connection, support for rekeying
+        // will be neccecary, but that's unlikely to happen.
+        if self.counter == 0 {
+            return Err(FrameError::NonceCounterWrapped);
+        }
+        let mut nonce = self.prefix.clone().to_vec();
+        nonce.append(&mut self.counter.clone().to_be_bytes().to_vec());
+
+        let nonce_l: [u8; NONCE_LENGTH] = nonce[..].try_into().unwrap();
+
+        self.inc();
+        Ok(nonce_l)
+    }
+
+    fn inc(&mut self) {
+        self.counter += 1;
+    }
+}
+
+
+impl std::error::Error for FrameError {}
+
+#[derive(Debug)]
+enum FrameError {
+    /// is the error returned when [`encode`] rejects the payload length.
+    InvalidPayloadLength(usize),
+
+    /// A cryptographic error occured.
+    Crypto(crypto_secretbox::Error),
+
+    /// An error occured with the I/O processing
+    IO(std::io::Error),
+
+    /// Returned when [`decode`] requires more data to continue.
+    EAgain,
+
+    /// Returned when [`decode`] failes to authenticate a frame.
+    TagMismatch,
+
+    /// Returned when the NaCl secretbox nonce's counter wraps (FATAL).
+    NonceCounterWrapped,
+
+    /// Returned when the buffer provided for writing a frame is too small.
+    ShortBuffer,
+}
+
+impl std::fmt::Display for FrameError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            FrameError::InvalidPayloadLength(s) => write!(f, "framing: Invalid payload length: {s}"),
+            FrameError::Crypto(e) => write!(f, "framing: Secretbox encrypt/decrypt error: {e}"),
+            FrameError::EAgain => write!(f, "framing: More data needed to decode"),
+            FrameError::TagMismatch => write!(f, "framing: Poly1305 tag mismatch"),
+            FrameError::NonceCounterWrapped => write!(f, "framing: Nonce counter wrapped"),
+            FrameError::ShortBuffer => write!(f, "framing: provided bytes buffer was too short for payload"),
+            FrameError::IO(e) => write!(f, "framing: i/o error occured while processing frame: {e}"),
+        }
+    }
+}
+
+impl From<crypto_secretbox::Error> for FrameError {
+    fn from(value: crypto_secretbox::Error) -> Self {
+        FrameError::Crypto(value)
+    }
+}
+
+impl From<std::io::Error> for FrameError {
+    fn from(value: std::io::Error) -> Self {
+        FrameError::IO(value)
     }
 }
 
 #[cfg(test)]
 mod testing;
-
 #[cfg(test)]
 mod generic_test;
