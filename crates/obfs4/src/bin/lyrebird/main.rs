d6eb62dac6dd148fe95e878bedfeff20db747441
jmwample <8297368+jmwample@users.noreply.github.com>
2024-03-06 23:21:19 -0700

    rebase issue resulted in files in the wrong place

--- crates/obfs4/src/bin/lyrebird/main.rs
+++ crates/obfs4/src/bin/lyrebird/main.rs
@@ -312,34 +312,3 @@ async fn main() -> Result<()> {
     }
     Ok(())
 }
-
-// //
-// //! Very very very basic soak test that runs lyrebird.
-//
-// use anyhow::Result;
-// use tokio;
-// use tor_ptmgr::ipc::{
-//     PluggableClientTransport, PluggableTransport, PtClientParameters, PtCommonParameters,
-// };
-// use tor_rtcompat::PreferredRuntime;
-// use tracing::info;
-//
-// #[tokio::main]
-// pub async fn main() -> Result<()> {
-//     tracing_subscriber::fmt::init();
-//     let common_params = PtCommonParameters::builder()
-//         .state_location("/tmp/arti-pt".into())
-//         .build()
-//         .unwrap();
-//     let client_params = PtClientParameters::builder()
-//         .transports(vec!["obfs4".parse().unwrap()])
-//         .build()
-//         .unwrap();
-//     let mut pt =
-//         PluggableClientTransport::new("./obfs4proxy".into(), vec![], common_params, client_params);
-//     pt.launch(PreferredRuntime::current()?).await?;
-//     loop {
-//         info!("message: {:?}", pt.next_message().await?);
-//     }
-//     Ok(())
-// }
