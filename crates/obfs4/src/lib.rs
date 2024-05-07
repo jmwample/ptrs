bcf19d030336dff67e8c557d5727b49a3cb41838
Jack Wampler <jmwample@users.noreply.github.com>
2024-05-07 15:36:09 -0600

    Fixes for Compilation on latest rust ver (#38)

--- crates/obfs4/src/lib.rs
+++ crates/obfs4/src/lib.rs
@@ -1,4 +1,7 @@
 #![doc = include_str!("../README.md")]
+// unexpected_cfgs are used to disable incomplete / WIP features and tests. This is
+// not an error for this library.
+#![allow(unexpected_cfgs)]
 
 // #![feature(trait_alias)]
 
