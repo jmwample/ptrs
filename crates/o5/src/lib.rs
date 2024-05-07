b6915abdb3bfb96ee1856288de36a4b3c22441a0
jmwample <8297368+jmwample@users.noreply.github.com>
2024-05-07 15:30:53 -0600

    latest rust ver dislikes unknown cfg targets

--- crates/o5/src/lib.rs
+++ crates/o5/src/lib.rs
@@ -1,5 +1,9 @@
 #![doc = include_str!("../README.md")]
 
+// unexpected_cfgs are used to disable incomplete / WIP features and tests. This is
+// not an error for this library.
+#![allow(unexpected_cfgs)]
+
 mod framing;
 // mod handshake;
 // mod transport;
--- crates/obfs4/src/lib.rs
+++ crates/obfs4/src/lib.rs
@@ -1,5 +1,9 @@
 #![doc = include_str!("../README.md")]
 
+// unexpected_cfgs are used to disable incomplete / WIP features and tests. This is
+// not an error for this library.
+#![allow(unexpected_cfgs)]
+
 // #![feature(trait_alias)]
 
 // #![allow(dead_code)]
